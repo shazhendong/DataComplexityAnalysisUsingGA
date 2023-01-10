3016f037f7742d91753f5f0e243e368e7f785cd6
Zhendong Sha <17443322+shazhendong@users.noreply.github.com>
2023-01-10 15:32:54 -0500

    move genetic algorithm to dataset folder

--- S2_PrepareHPC.sh
+++ S2_PrepareHPC.sh
@@ -1,1 +1,24 @@
 # The pipeline specified in this script is used to prepare for HPC computation.
+# Author: Zhendong Sha@2023-01-09
+
+#### Parameters ####
+dir_scr=scr
+dir_res=res
+
+#### Prepare genetic algrithms scr ####
+echo "Prepare genetic algrithms scr"
+
+# copy GA to GWAS folder
+cp -r $dir_scr/GeneticAlgorithm/*.py $dir_res/GWAS
+
+# copy GA to GAMETES folder
+for folder in $dir_res/GAMETES/*
+do
+    cp -r $dir_scr/GeneticAlgorithm/*.py $folder
+done
+
+# copy GA to GEO folder
+for folder in $dir_res/GEO_Datasets/*
+do
+    cp -r $dir_scr/GeneticAlgorithm/*.py $folder
+done
