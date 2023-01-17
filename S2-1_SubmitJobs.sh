940670cc42c254f5a6b3db267b0ec20ad7c4b65e
Zhendong Sha <17443322+shazhendong@users.noreply.github.com>
2023-01-16 23:11:11 -0500

    Update S2-1_SubmitJobs.sh

--- S2-1_SubmitJobs.sh
+++ S2-1_SubmitJobs.sh
@@ -1,6 +1,8 @@
 echo "Processing GWAS folder"
 
-cd res/GWAS
+target_folder="res"
+
+cd ${target_folder}/GWAS
 for file in *.sh
 do
     echo "Submitting job $file"
@@ -10,7 +12,7 @@ cd ..
 cd ..
 
 # iterate over folders under /res folder
-for folder in res/GAMETES/*
+for folder in ${target_folder}/GAMETES/*
 do
     # iterate over files under each folder
     echo "Processing folder $folder"
@@ -26,7 +28,7 @@ do
     cd ..
 done
 
-for folder in res/GEO_Datasets/*
+for folder in ${target_folder}/GEO_Datasets/*
 do
     # iterate over files under each folder
     echo "Processing folder $folder"
