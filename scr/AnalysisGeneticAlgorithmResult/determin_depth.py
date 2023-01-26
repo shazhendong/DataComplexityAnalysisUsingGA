305251966908dd4ffdf999630c68ede6d85e965c
Zhendong Sha <17443322+shazhendong@users.noreply.github.com>
2023-01-26 11:53:02 -0500

    Update determin_depth.py
    add kneed

--- scr/AnalysisGeneticAlgorithmResult/determin_depth.py
+++ scr/AnalysisGeneticAlgorithmResult/determin_depth.py
@@ -28,7 +28,32 @@ auc_df_max_995 = auc_df_max * 0.995
 # get 100% of the max for each column of auc_df
 auc_df_max_100 = auc_df_max * 1.0
 
+#### Determin the depth of train, test, and validation AUCs using kneed ####
+from kneed import KneeLocator
+# get the size_lim as the x-axis
+x = auc_df.index.values
+# transform x into list of float
+x = [float(i) for i in x]
+# get the train, test, and validation AUCs as the y-axis
+y_train = auc_df['auc_roc_train'].values
+y_test = auc_df['auc_roc_test'].values
+y_validation = auc_df['auc_roc_validation'].values
+# transform y_train, y_test, and y_validation into list of float
+y_train = [float(i) for i in y_train]
+y_test = [float(i) for i in y_test]
+y_validation = [float(i) for i in y_validation]
+# get the depth of train, test, and validation AUCs using kneed
+kneedle_train = KneeLocator(x, y_train, S=1.0, curve='concave', direction='increasing')
+kneedle_test = KneeLocator(x, y_test, S=1.0, curve='concave', direction='increasing')
+kneedle_validation = KneeLocator(x, y_validation, S=1.0, curve='concave', direction='increasing')
+# get the kneedle size_lim for train, test, and validation AUCs
+kneedle_train_size_lim = kneedle_train.knee
+kneedle_test_size_lim = kneedle_test.knee
+kneedle_validation_size_lim = kneedle_validation.knee
 
+
+
+#### Determin the depth of train, test, and validation AUCs that reach 90%, 95%, 99%, 99.5%, and 100% of the max ####
 # get the value of the smallest size_lim for each column of auc_df that reaches 90% of the max
 auc_df_90 = auc_df[auc_df >= auc_df_max_90]
 # split auc_df_90 into three dataframes for train, test, and validation AUCs
@@ -85,16 +110,17 @@ auc_df_100_test_size_lim = auc_df_100_test.first_valid_index()
 auc_df_100_validation_size_lim = auc_df_100_validation.first_valid_index()
 
 
-# merge the size_lim for each column of auc_df that reaches 90%, 95%, 99%, 99.5% and max of the max with train, test, and validation as the column names
+# merge the size_lim for each column of auc_df that reaches 90%, 95%, 99%, 99.5%, max and kneed of the max with train, test, and validation as the column names
 auc_df_90_size_lim = pd.DataFrame({'train': [auc_df_90_train_size_lim], 'test': [auc_df_90_test_size_lim], 'validation': [auc_df_90_validation_size_lim]}) 
 auc_df_95_size_lim = pd.DataFrame({'train': [auc_df_95_train_size_lim], 'test': [auc_df_95_test_size_lim], 'validation': [auc_df_95_validation_size_lim]})
 auc_df_99_size_lim = pd.DataFrame({'train': [auc_df_99_train_size_lim], 'test': [auc_df_99_test_size_lim], 'validation': [auc_df_99_validation_size_lim]})
 auc_df_995_size_lim = pd.DataFrame({'train': [auc_df_995_train_size_lim], 'test': [auc_df_995_test_size_lim], 'validation': [auc_df_995_validation_size_lim]})
 auc_df_100_size_lim = pd.DataFrame({'train': [auc_df_100_train_size_lim], 'test': [auc_df_100_test_size_lim], 'validation': [auc_df_100_validation_size_lim]})
-# merge the three dataframes into one dataframe
-auc_df_size_lim = pd.concat([auc_df_90_size_lim, auc_df_95_size_lim, auc_df_99_size_lim, auc_df_995_size_lim, auc_df_100_size_lim], ignore_index=True)
+auc_df_kneed_size_lim = pd.DataFrame({'train': [kneedle_train_size_lim], 'test': [kneedle_test_size_lim], 'validation': [kneedle_validation_size_lim]})
+# merge the these dataframes into one dataframe
+auc_df_size_lim = pd.concat([auc_df_90_size_lim, auc_df_95_size_lim, auc_df_99_size_lim, auc_df_995_size_lim, auc_df_100_size_lim, auc_df_kneed_size_lim], ignore_index=True)
 # rename the index of auc_df_size_lim
-auc_df_size_lim.index = ['90%', '95%', '99%', '99.5%', '100%']
+auc_df_size_lim.index = ['90%', '95%', '99%', '99.5%', '100%', 'kneedle']
 # write auc_df_size_lim to a csv file
 auc_df_size_lim.to_csv(output_file)
 
